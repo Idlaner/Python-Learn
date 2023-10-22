015ec2ab12236e97637b9adb9dd948e83cf5ff06
Idlaner <108646078+Idlaner@users.noreply.github.com>
2023-10-22 23:27:23 +0700

    Guessing Game.py

--- Guessing Game.py
+++ Guessing Game.py
@@ -1,5 +1,6 @@
 #Guess the answer using loop
-secret_person = "Bruno"
+print("This is a Manchester United player")
+secret_person = "Harry Maguire"
 
 guess = ""
 guess_count = 0
@@ -7,7 +8,7 @@ guess_limit = 5
 outof_guess = False
 while guess != secret_person and not(outof_guess):
     if guess_count < guess_limit:
-        guess = str(input("Choose a person: "))
+        guess = str(input("Guess a player: "))
         guess_count = guess_count + 1
     else:
         outof_guess = True
