10f36489e60476f76a3a90067ca51687235b2808
Idlaner <108646078+Idlaner@users.noreply.github.com>
2023-10-22 23:10:53 +0700

    Add files via upload

---
+++ BasicCalc.py
@@ -0,0 +1,17 @@
+#Create basic Calculator
+num1 = float(input("Enter first number: "))
+opr = input("Enter operator: ")
+num2 = float(input("Enter second number: "))
+
+if opr == "+":
+    print(num1 + num2)
+elif opr == "-":
+    print(num1 - num2)
+elif opr == "/":
+    print(num1 / num2)
+elif opr == "*":
+    print(num1 * num2)
+elif opr == "%":
+    print(num1 % num2)
+else:
+    print("Invalid calculation: Another operations are under development")
---
+++ Guessing Game.py
@@ -0,0 +1,17 @@
+#Guess the answer using loop
+secret_person = "Bruno"
+
+guess = ""
+guess_count = 0
+guess_limit = 5
+outof_guess = False
+while guess != secret_person and not(outof_guess):
+    if guess_count < guess_limit:
+        guess = str(input("Choose a person: "))
+        guess_count = guess_count + 1
+    else:
+        outof_guess = True
+if outof_guess:
+    print("You lose!")
+else:
+    print("Congratulations!")
